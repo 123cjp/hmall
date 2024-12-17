f47436261b6d8eea104dbe116a2caf1f628875ba
陈建平 <19937325873@163.com~>
2024-12-17 08:30:32 +0800

    网关配置

--- pay-service/src/main/java/com/cjp/pay/service/IPayOrderService.java
+++ pay-service/src/main/java/com/cjp/pay/service/IPayOrderService.java
@@ -11,8 +11,8 @@ import com.cjp.pay.domain.po.PayOrder;
  * 支付订单 服务类
  * </p>
  *
- * @author 虎哥
- * @since 2023-05-16
+ * @author cjp
+ * @since
  */
 public interface IPayOrderService extends IService<PayOrder> {
 
